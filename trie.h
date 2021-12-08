6f08ed46fd554cf2569137e70d29c2a450300c6e
Ariel Warren <arieldovidwarren@gmail.com>
2021-12-08 19:09:29 +0200

    lets go

--- trie.h
+++ trie.h
@@ -1,1 +1,30 @@
 #pragma once
+
+
+class Trie 
+{
+protected:
+    class TrieNode
+    {
+    public:
+        TrieNode* children[26] = { 0 };
+        TrieNode* father;
+        bool isEndWord = false;
+        //int countChildren = 0;
+    public:
+        TrieNode();
+    };
+        TrieNode* root;
+  public:
+        Trie() {}//ctor - initialize root to be "root"
+
+        //all functions
+        void insertWord(string str);
+        void deleteWord(string str);
+        bool searchWord(string str);
+        void printAutoSuggestions(TrieNode*);
+    private:
+        bool searchWord(string str, TrieNode* node);
+
+   
+    };

d0a0269e73889c6f9c87550f00d399894329fc3d
yossi tyberg <tybergyossi@gmail.com>
2021-12-08 19:09:53 +0200

    ok

--- trie.h
+++ trie.h
@@ -1,1 +1,26 @@
 #pragma once
+const int ALPHABET = 26;
+class Trie {
+protected:
+	
+	class TrieNode {
+	public:
+		TrieNode* children[ALPHABET] = { 0 };
+		TrieNode* parent;
+		bool isEndWord = false;
+
+		TrieNode() {}
+	};
+
+	TrieNode* root;
+public:
+	Trie() {}
+
+	void insertWord(string str);
+	void deleteWord(string str);
+	bool searchWord(string str);
+	void printAllWordsFromPrefix(TrieNode*);
+private:
+	bool searchWord(string str, TrieNode* node);
+};
+
